0e90bfec1ef7029b82fa688206322a86ac9505e1
Martin Schreurs <martin.schreurs@netcologne.de>
2023-07-04 19:24:31 +0200

    reformat

--- Print.py
+++ Print.py
@@ -6,13 +6,12 @@
 from enum import Enum
 
 
-
 class PrintType(Enum):
     INFO = "[INFO]",
     ERROR = "[ERROR]",
-    FINISH =  "[FINISH]",
+    FINISH = "[FINISH]",
 
 
 def Print(message: str, type: PrintType) -> None:
-    longest: int = len(PrintType.FINISH.value[0]) # enter the longest enum here
+    longest: int = len(PrintType.FINISH.value[0])  # enter the longest enum here
     print(f"{type.value[0]}{' ' * (longest - len(type.value[0]))} {message}")
